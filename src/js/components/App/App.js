f811721a74508e8d58622e41388e930a74c1617c
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2018-10-19 11:20:56 +0100

    Add Tableau Api options

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -5,13 +5,37 @@ import tableau from 'tableau-api';
 class App extends Component{
 
     componentDidMount(){
-        this.initViz();
+        this.initTableau();
     }
 
-    initViz(){
-        const vizUrl = 'https://public.tableau.com/views/RegionalSampleWorkbook/Storms';  
+    initTableau(){
+        const vizUrl = 'https://public.tableau.com/views/RegionalSampleWorkbook/Storms?:embed=y&:display_count=yes';
+        const options = {
+            hideTabs: false,
+            // width: "700px",
+            // height: "800px",
+            onFirstInteractive: () => {
+                const sheet = viz.getWorkbook().getActiveSheet().getWorksheets().get("Table");
+                const options = {
+                    ignoreAliases: false,
+                    ignoreSelection: false,
+                    includeAllColumns: false
+                };
+                sheet.getUnderlyingDataAsync(options).then((t) => {
+                    const tableauData = t.getData();
+                    let data = [];
+                    const pointCount = tableauData.length;
+                    for(let a = 0; a < pointCount; a++ ) {
+                        data = data.concat({
+                            x: tableauData[a][0].value,
+                            y: Math.round(tableauData[a][3].value,2)
+                        })
+                    };
+                })
+            }
+        };  
         const vizContainer = this.vizContainer;  
-        let viz = new window.tableau.Viz(vizContainer, vizUrl)  
+        let viz = new window.tableau.Viz(vizContainer, vizUrl, options)  
     }
     
     render(){
